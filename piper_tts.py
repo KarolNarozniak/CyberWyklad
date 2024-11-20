48f2d65a1fccf04b17b53d5d998d3a29b56c2b66
Szymon Rosowski <rosowskiszymon@gmail.com>
2024-11-20 02:26:29 +0100

    fix broken json in request body

--- piper_tts.py
+++ piper_tts.py
@@ -6,10 +6,12 @@ class PiperTTS:
         self.url = url
 
     def get_audio(self, text: str) -> bytes:
-        response = requests.post(self.url, data='{"text": "'+f'{text.strip()}'+'}')
+        data={"text": text}
+        response = requests.post(self.url, json=data)
         return response.content
 
     def save_to_file(self, text: str, file_path: str) -> None:
         audio = self.get_audio(text)
         with open(file_path, 'wb') as file:
             file.write(audio)
+
